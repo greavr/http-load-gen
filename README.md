bf8074ea4235f4901cdf2536fae70098936e19f2
Rick Greaves <rgreaves@google.com>
2022-09-30 00:35:57 -0700

    Tadaa

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # http-load-gen
 Http Load Gen
+
+## Run Code Locally
+```
+pip3 install virtualenv
+python3 -m virtualenv venv
+source venv/bin/activate
+pip3 install -r src/requirements.txt
+python3 src/main.py
+```
+
+**Deactivate the environment** 
+Run the following command
+```
+deactivate
+```
+## Environment Variables ##
+The following environment variables can be set:
+  - **TARGET_URL** - REQUIRED, should be fully formed url, example: http://google.com
+  - **AVERAGE_RPS** - Baseline request per second count, default is **10** (RPS)
+  - **MAX_RPS** - Max request per second count, default is **100** (RPS)
+  - **DURATION** - Time from Average to Max, default is **60** (in seconds)
+  - **LOOPS** - INT Of number of cycles, defaults to **0** which is forever
+
---
+++ src/main.py
@@ -0,0 +1,59 @@
+import threading
+import time
+import sys
+import os
+import requests
+
+target_url = os.getenv('TARGET_URL')
+average_rps = int(os.environ.get('AVERAGE_RPS', 10))
+max_rps = int(os.environ.get('MAX_RPS', 100))
+cycle_duration = int(os.environ.get('DURATION', 60))
+loop_count = int(os.environ.get('LOOPS', 0))
+
+# Function to make request
+def make_request(request_id):
+    """ Make Request """
+    x = requests.get(url = target_url, verify=False)
+    print(f"{request_id}: {x.status_code}")
+
+if __name__ == "__main__":
+    """ Main Entry Point"""
+
+    if not target_url:
+        # Validate required parameter
+        print(f"TARGET_URL not set, exiting")
+        sys.exit(os.EX_DATAERR)
+
+    # Output running config
+    print(f"""
+    Running HTTP LoadGen
+    Target: {target_url}
+    Avg RPS: {average_rps}
+    Max RPS: {max_rps}
+    Cycle Duration: {cycle_duration}
+    Loop Count: {loop_count}
+    """)
+
+    if loop_count != 0:
+        loop_num = 0
+    else:
+        loop_num = 1
+
+    total_requests = 0 
+    growth_per_loop = round(((max_rps-average_rps) / cycle_duration))
+    current_thread_count = average_rps
+
+    while loop_num != loop_count:
+        print(f"Loop: {loop_num+1}/{loop_count}")
+        for i in range(current_thread_count):
+            print(f"Request: {i+1}/{current_thread_count}")
+            x = threading.Thread( target=make_request, args=(i,))
+            x.start
+            total_requests += 1
+
+        current_thread_count += growth_per_loop
+        loop_num += 1
+        time.sleep(1)
+
+    # Loop complete
+    print(f"Completed a total of {total_requests} request(s)")
